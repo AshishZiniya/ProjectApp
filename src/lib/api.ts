f469dd66942ba4c3ea2d2f95919015bc40303f68
Ziniya Ashish <98575396+AshishZiniya@users.noreply.github.com>
2025-09-10 18:01:02 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "projectapp-api.netlify.app";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
